c4e30ffa12d91e68b336948c30e5c2c3cbbefec0
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-12 23:13:33 +0100

    Implementing about section

---
+++ front-end/src/patterns/about-section/index.js
@@ -0,0 +1,20 @@
+import Image from '@/components/image'
+import styles from './about.module.css'
+
+export default function AboutSection() {
+    return <section id="about">
+
+        <h2>About Me</h2>
+        <div className={styles['container']}>
+            <ul className={styles.left}>
+                <li>Programador Java e NextJS.</li>
+                <li>Atualmente a trabalhar como desenvolvedor de aplicações na Eviden Portugal.</li>
+                <li>Licenciado em Engenharia Informatica na Universidade do Algarve em 2022</li>
+            </ul>
+
+            <div className={styles.right}>
+                <Image className={styles.image} src={"/images/me.jpg"} alt="Mauro photo" />
+            </div>
+        </div>
+    </section>
+}
