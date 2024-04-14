41787fa752c0b7f233433544deabf07692b2c8a8
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-14 15:07:52 +0100

    Refactor

--- front-end/src/patterns/skills-section/index.js
+++ front-end/src/patterns/skills-section/index.js
@@ -1,46 +1,20 @@
 import SkillItem from '../skill-item'
 import styles from './skills.module.css'
-
-const SKILLS = [{
-  icon: "/images/java-icon.png",
-  skill: "Java",
-  level: 80
-}, {
-  icon: "/images/react-icon.svg.png",
-  skill: "React JS / Next JS",
-  level: 90
-},
-{
-  icon: "/images/html-icon.png",
-  skill: "HTML",
-  level: 70
-},
-{
-  icon: "/images/css-icon.png",
-  skill: "CSS",
-  level: 70
-},
-{
-  icon: "/images/python-logo-only.png",
-  skill: "Python",
-  level: 40
-},
-{
-  icon: "/images/mysql-icon.png",
-  skill: "MySQL",
-  level: 40
-}]
+import skills from './skills.json'
+import WidthLimitedSection from '@/components/widthLimitedSection'
 
 export default function SkillsSection() {
   return (
-    <section className={styles.section} id='skills'>
+    <WidthLimitedSection className={styles.section} id='skills'>
       <h2>Skills</h2>
 
       <div className={styles.skills}>
-        {SKILLS.sort((a,b) => b.level - a.level).map(skill => (
-          <SkillItem key={skill.name}  {...skill} />
-        ))}
+        {skills
+          .sort((a, b) => b.level - a.level)
+          .map((skill) => (
+            <SkillItem key={skill.name} {...skill} />
+          ))}
       </div>
-    </section>
+    </WidthLimitedSection>
   )
 }
