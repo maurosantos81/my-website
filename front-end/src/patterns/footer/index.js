31012ac13c5138bbf934fd3bfc1b68580bde32b7
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-15 15:46:47 +0100

    Implementing footer

--- front-end/src/patterns/footer/index.js
+++ front-end/src/patterns/footer/index.js
@@ -1,13 +1,35 @@
 import { Divider } from '@mui/material'
 import styles from './footer.module.css'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub'
+import Link from '@/components/link'
+
+const SOCIAL_MEDIAS = [
+  {
+    icon: <LinkedInIcon />,
+    href: 'https://linkedin.com/in/maurosantos81/',
+  },
+  {
+    icon: <GitHubIcon />,
+    href: 'https://github.com/maurosantos81/',
+  },
+]
 
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <Divider sx={{ borderBottomWidth: 'medium' }} />
-      <div className={styles['container']}>
-        <p>© Mauro Santos</p>
-        <div className={styles['social-medias']}></div>
+      <div className='limit-width'>
+        <Divider />
+        <div className={styles['container']}>
+          <p>© Mauro Santos, 2024</p>
+          <div className={styles['social-medias']}>
+            {SOCIAL_MEDIAS.map((socialMedia) => (
+              <Link key={socialMedia.href} href={socialMedia.href}>
+                {socialMedia.icon}
+              </Link>
+            ))}
+          </div>
+        </div>
       </div>
     </footer>
   )
