f3ff6654af1b09c1e63a186a2e340cda4ad4b581
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-18 13:11:07 +0100

    Removing console warning

--- front-end/src/components/textFieldController/index.js
+++ front-end/src/components/textFieldController/index.js
@@ -1,20 +1,21 @@
 import { Controller } from 'react-hook-form'
 import TextField from '../textField'
+import { forwardRef } from 'react'
 
-export default function TextFieldController({
-  name,
-  control,
-  rules,
-  controllerProps,
-  ...props
-}) {
-  return (
-    <Controller
-      name={name}
-      control={control}
-      rules={rules}
-      {...controllerProps}
-      render={({ field }) => <TextField {...field} {...props} />}
-    />
-  )
-}
+const TextFieldController = forwardRef(
+  ({ name, control, rules, controllerProps, ...props }, ref) => {
+    return (
+      <Controller
+        name={name}
+        control={control}
+        rules={rules}
+        {...controllerProps}
+        render={({ field }) => <TextField {...field} {...props} ref={ref} />}
+      />
+    )
+  },
+)
+
+TextFieldController.displayName = 'TextFieldController'
+
+export default TextFieldController
