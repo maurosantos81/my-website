e25941a76213369a9e5af69ec2a0d4849a620a58
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-24 11:47:07 +0100

    internationalization of sitemap

--- front-end/src/app/sitemap.js
+++ front-end/src/app/sitemap.js
@@ -1,8 +1,21 @@
+import { defaultLanguage, locales } from '@/constants/locales'
+
+const host = 'https://maurosantos.xyz'
+
 export default function sitemap() {
+  function getUrl(locale) {
+    return `${host}/${locale}/`
+  }
+
   return [
     {
-      url: 'https://maurosantos.xyz',
-      priority: 1,
+      url: host,
+      lastModified: new Date(),
+      alternates: {
+        languages: Object.fromEntries(
+          locales.map((locale) => [locale, getUrl(locale)]),
+        ),
+      },
     },
   ]
 }
