72e61050fcc1820fffa803de07225943ca5b6ca0
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-24 10:40:42 +0100

    Static translation

--- front-end/src/app/[locale]/page.js
+++ front-end/src/app/[locale]/page.js
@@ -4,8 +4,11 @@ import PortfolioSection from '@/patterns/portfolio/portfolio-section'
 import HireMeSection from '@/patterns/hire-me/hire-me-section'
 import HeroSection from '@/patterns/hero-section'
 import SkillsSection from '@/patterns/skills/skills-section'
+import { unstable_setRequestLocale } from 'next-intl/server'
 
-export default async function Home({ params: { lang } }) {
+export default async function Home({ params: { locale } }) {
+  unstable_setRequestLocale(locale)
+
   return (
     <main className={styles.home}>
       <HeroSection />
