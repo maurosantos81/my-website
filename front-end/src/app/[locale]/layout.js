791259199da5e7948f618df8a11c7e2f94a58a13
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-04-24 10:40:54 +0100

    Static translation + translate metadata

--- front-end/src/app/[locale]/layout.js
+++ front-end/src/app/[locale]/layout.js
@@ -2,56 +2,72 @@ import '../globals.css'
 import Navbar from '@/patterns/navbar'
 import Footer from '@/patterns/footer'
 import { NextIntlClientProvider, useMessages } from 'next-intl'
+import { locales } from '@/constants/locales'
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server'
 
-const DESCRIPTION =
-  "Elevate your projects with a Java + Next.js Junior Full-stack Developer! With a passion for coding, I'm here to bring fresh perspectives and innovation to every task. From robust backend solutions to stylish frontend interfaces, I'll turn your ideas into reality!"
+export async function generateMetadata({ params: { locale } }) {
+  const t = await getTranslations({ locale, namespace: 'Metadata' })
 
-export const metadata = {
-  title: 'Mauro Santos - Full Stack Developer',
-  authors: [{ name: 'Mauro Santos' }],
-  creator: 'Mauro Santos',
-  publisher: 'Mauro Santos',
-  description: DESCRIPTION,
-  openGraph: {
-    title: 'Mauro Santos',
-    description: DESCRIPTION,
-    images: ['/images/logo.png'],
-    authors: ['Mauro Santos'],
-  },
-  applicationName: 'Mauro Santos',
-  keywords: [
-    'Mauro Santos',
-    'React',
-    'Next.js',
-    'Java',
-    'Developer',
-    'Full-Stack',
-    'Full stack',
-    'Website development',
-    'Web development',
-    'Backend developer',
-    'Frontend developer',
-    'HTML',
-    'CSS',
-    'Portfolio',
-    'Projects',
-    'Junior',
-    'Programmer',
-  ],
-  category: 'technology',
+  return {
+    title: t('title'),
+    authors: [{ name: 'Mauro Santos' }],
+    creator: 'Mauro Santos',
+    publisher: 'Mauro Santos',
+    description: t('description'),
+    openGraph: {
+      title: t('title'),
+      siteName: t('title'),
+      description: t('description'),
+      locale: { locale },
+      images: ['/images/logo.png'],
+      authors: ['Mauro Santos'],
+      type: 'website',
+    },
+    applicationName: 'Mauro Santos',
+    keywords: [
+      'Mauro Santos',
+      'React',
+      'Next.js',
+      'Java',
+      'Developer',
+      'Full-Stack',
+      'Full stack',
+      'Website development',
+      'Web development',
+      'Backend developer',
+      'Frontend developer',
+      'HTML',
+      'CSS',
+      'Portfolio',
+      'Projects',
+      'Junior',
+      'Programmer',
+    ],
+    category: t('category'),
+    twitter: {
+      card: 'summary_large_image',
+      title: t('title'),
+      description: t('description'),
+      images: {
+        url: '/images/logo.png',
+        alt: 'Mauro Santos Logo',
+      },
+    },
+  }
 }
 
-// export async function generateStaticParams() {
-//   return [{ locale: 'en' }, { locale: 'pt' }]
-// }
+export function generateStaticParams() {
+  return locales.map((locale) => ({ locale }))
+}
 
-function RootLayout({ children, params }) {
+function RootLayout({ children, params: { locale } }) {
   const messages = useMessages()
+  unstable_setRequestLocale(locale)
 
   return (
-    <html lang={params.locale}>
+    <html lang={locale}>
       <body>
-        <NextIntlClientProvider locale={params.locale} messages={messages}>
+        <NextIntlClientProvider locale={locale} messages={messages}>
           <Navbar />
           {children}
         </NextIntlClientProvider>
