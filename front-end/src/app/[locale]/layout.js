bc8a5375db8079033ecc3481bc82b93761e30676
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-05-07 17:22:32 +0100

    fix scroll bug when cofettis are on screen

--- front-end/src/app/[locale]/layout.js
+++ front-end/src/app/[locale]/layout.js
@@ -53,6 +53,10 @@ export async function generateMetadata({ params: { locale } }) {
         alt: 'Mauro Santos Logo',
       },
     },
+    other: {
+      viewport:
+        'height=device-height, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no, target-densitydpi=device-dpi',
+    },
   }
 }
 
