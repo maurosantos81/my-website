2077e48126e14147ba44cb5b7fb55c811826a56c
Mauro Santos <41169648+maurosantos81@users.noreply.github.com>
2024-05-13 14:12:29 +0100

    Implementing language switch button

---
+++ front-end/src/patterns/languageSwitcher/index.js
@@ -0,0 +1,97 @@
+'use client'
+
+import { useLocale } from 'next-intl'
+import { useEffect, useRef, useState } from 'react'
+import styles from './languageSwitcher.module.css'
+import { motion, AnimatePresence } from 'framer-motion'
+import { locales } from '@/constants/locales'
+import Image from 'next/image'
+import Link from 'next/link'
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
+
+const itemVariants = {
+  open: {
+    opacity: 1,
+    y: 0,
+    transition: { type: 'spring', stiffness: 300, damping: 24 },
+  },
+  closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+}
+
+const hoverColor = '#e3e3e3'
+
+export default function LanguageSwitcher({ className = '' }) {
+  const locale = useLocale()
+  const [isOpen, setIsOpen] = useState(false)
+  const ref = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('click', handleClickOutside)
+    } else {
+      document.removeEventListener('click', handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside)
+    }
+  }, [isOpen])
+
+  return (
+    <div ref={ref} className={className} style={{ position: 'relative' }}>
+      <div
+        className={styles.languageSwitcher}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <Image
+          width={30}
+          height={30}
+          src={`/images/${locale}-flag.webp`}
+          alt={locale === 'en' ? 'English' : 'Portuguese'}
+        />
+        <KeyboardArrowDownIcon style={{ color: '#fff' }} fontSize='12px' />
+      </div>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.ul
+            className={styles.ul}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.25 }}
+            exit={{ opacity: 0 }}
+          >
+            {locales
+              .filter((localeItem) => localeItem !== locale)
+              .map((localeItem) => (
+                <Link key={localeItem} href={`/${localeItem}/`}>
+                  <motion.li
+                    className={styles.item}
+                    variants={itemVariants}
+                    initial='closed'
+                    animate={isOpen ? 'open' : 'closed'}
+                    whileHover={{
+                      backgroundColor: hoverColor,
+                      borderColor: hoverColor,
+                      transition: { duration: 0.5 },
+                    }}
+                  >
+                    <Image
+                      width={32}
+                      height={21}
+                      src={`/images/${localeItem}-flag.webp`}
+                      alt={localeItem === 'en' ? 'English' : 'Portuguese'}
+                    />
+                  </motion.li>
+                </Link>
+              ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
--- front-end/src/patterns/navbar/index.js
+++ front-end/src/patterns/navbar/index.js
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import styles from './navbar.module.css'
 import useScrollTo from '@/hooks/useScrollTo'
 import { useTranslations } from 'next-intl'
+import LanguageSwitcher from '../languageSwitcher'
 
 export default function Navbar() {
   const { scrollTo } = useScrollTo()
@@ -11,12 +12,16 @@ export default function Navbar() {
 
   return (
     <nav className={`${styles.container}`}>
-      <Image height={50} width={50} src='/images/logo.png' alt='icon' />
+      <div className={styles['icon-container']}>
+        <Image height={50} width={50} src='/images/logo.webp' alt='icon' />
+        <LanguageSwitcher className={styles['icon-lang-switcher']} />
+      </div>
 
       <div className={styles.right}>
         <p onClick={() => scrollTo('about')}>{t('about')}</p>
         <p onClick={() => scrollTo('skills')}>{t('skills')}</p>
         <p onClick={() => scrollTo('portfolio')}>{t('portfolio')}</p>
+        <LanguageSwitcher className={styles['lang-switcher']} />
         <p onClick={() => scrollTo('hire-me')} className={styles['hire-me']}>
           {t('hire-me')}
         </p>
